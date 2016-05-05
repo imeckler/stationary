67b394e99ab211e5cec2ad985edbde57498d2cec
Izaak Meckler <ihmeckler@gmail.com>
2016-05-05 14:36:17 -0400

    _

--- src/stationary.ml
+++ src/stationary.ml
@@ -1,2 +1,5 @@
-module Std = Std
-include Std
+module Html        = struct include Html end
+module Attribute   = struct include Attribute end
+module File        = struct include File end
+module File_system = struct include File_system end
+module Site        = struct include Site end
